35e821fdecea0c328dc0ab42e5c9e28bc0717ae4
Steve Gossip <stephengossip@gmail.com>
2019-07-23 17:48:04 +0100

    MVP completed. all tests passing

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -2,12 +2,24 @@ import java.util.ArrayList;
 
 public class Library {
     private ArrayList<Book> shelf;
+    private int capacity;
 
-    public Library() {
+    public Library(int capacity) {
         this.shelf = new ArrayList<Book>();
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
     }
 
     public int bookCount() {
         return this.shelf.size();
+    }
+
+    public void addBook(Book book) {
+        if (this.shelf.size() < this.capacity) {
+            this.shelf.add(book);
+        }
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -6,15 +6,35 @@ import static org.junit.Assert.assertEquals;
 public class LibraryTest {
 
     private Library library;
+    private Book book;
 
     @Before
     public void before() {
-        library = new Library();
+        library = new Library(1);
+        book = new Book();
     }
 
     @Test
     public void libraryStartsEmpty() {
         assertEquals(0, library.bookCount());
+    }
+
+    @Test
+    public void canAddBookToShelf() {
+        library.addBook(book);
+        assertEquals(1, library.bookCount());
+    }
+
+    @Test
+    public void libraryHasCapacity() {
+        assertEquals(1, library.getCapacity());
+    }
+
+    @Test
+    public void cannotAddIfCapacityReached() {
+        library.addBook(book);
+        library.addBook(book);
+        assertEquals(1, library.bookCount());
     }
 
 }
